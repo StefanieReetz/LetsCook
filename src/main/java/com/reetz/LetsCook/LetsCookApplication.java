96bf9cb4b92a047fcc9f12961de4ecd8afe55e2c
StefanieReetz <stefaniebeatrizreetz@gmail.com>
2025-04-13 07:00:44 -0300

    Initial commit

---
+++ src/main/java/com/reetz/LetsCook/LetsCookApplication.java
@@ -0,0 +1,13 @@
+package com.reetz.LetsCook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsCookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsCookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reetz/LetsCook/LetsCookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reetz.LetsCook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetsCookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
