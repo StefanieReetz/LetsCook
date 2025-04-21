52f215bf4398925547d9a8388bdf31a096b0f2ca
StefanieReetz <stefaniebeatrizreetz@gmail.com>
2025-04-20 23:26:31 -0300

    Favorited

--- src/main/java/com/reetz/LetsCook/repository/FavoritedRecipeRepository.java
+++ src/main/java/com/reetz/LetsCook/repository/FavoritedRecipeRepository.java
@@ -2,10 +2,12 @@ package com.reetz.LetsCook.repository;
 
 import com.reetz.LetsCook.entity.FavoritedRecipe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface FavoritedRecipeRepository extends JpaRepository<FavoritedRecipe, Long> {
     List<FavoritedRecipe> findByUsuarioId(Long usuarioId);
 
